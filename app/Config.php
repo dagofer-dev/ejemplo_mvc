faf0debf7ee61b5c7e5d528593c38396ad1fe615
dagofer-dev <daniel.godoy.profe@gmail.com>
2025-12-22 09:28:08 +0100

    Cambios de estilo
    Se han hecho algunos cambios de estilo. Se ha añadido el usuario y contraseña de base de datos.

--- app/Config.php
+++ app/Config.php
@@ -2,8 +2,8 @@
 class Config {
 	static public $mvc_bd_hostname = 'localhost';
 	static public $mvc_bd_nombre = 'alimentos';
-	static public $mvc_bd_usuario = '';
-	static public $mvc_bd_clave = '';
+	static public $mvc_bd_usuario = 'dani';
+	static public $mvc_bd_clave = 'dani';
 	static public $mvc_vis_css = 'estilo.css';
 }
 ?>
